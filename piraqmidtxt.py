a3df9d464e81d676bdbdf9033ee0d9973fa4d11b
Doni Andrian <91331154+BlackArh08@users.noreply.github.com>
2021-09-25 16:57:28 +0700

    Add files via upload

---
+++ piraqmidtxt.py
@@ -0,0 +1,10 @@
+i = 1
+n = 3
+while i <= n:
+    j = i
+    while j >= 1:
+         print(j, end=' ')
+         j = j - 1
+    print("\r")
+    i += 1
+
---
+++ sms.py
@@ -0,0 +1,7 @@
+from twilio.rest import Client
+
+client=Client("ACe58d39973b3fcd5f29e399dd0202d128","59ef4b2fd0cdea8769075a37cd372dc1")
+
+client.messages.create(to =["+6281934172542"],
+    from_= "+12157128143",
+    body="Selamat PUTRA!! ANDA TELAH RESMI MENJADI RAJA  SEDUNIA ")
